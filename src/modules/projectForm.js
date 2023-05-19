ddbceb5c83f1866744f02d2f9b55046df4d92552
elsie <elsiechen915@gmail.com>
2023-05-19 09:20:58 -0400

    Rearrange code for readability

--- src/modules/projectForm.js
+++ src/modules/projectForm.js
@@ -1,27 +1,73 @@
-const projectForm = document.createElement('div');
-const input = document.createElement('input');
-const btnContainer = document.createElement('div');
-const createBtn = document.createElement('button');
-const cancelBtn = document.createElement('button');
+const sidebar = document.querySelector('.sidebar');
+const addProjectBtn = document.querySelector('.add-project');
 
-projectForm.classList.add('projectForm');
-btnContainer.classList.add('btnContainer');
+const ProjectForm = () => {
+    const projectForm = document.createElement('div');
+    const input = document.createElement('input');
+    const btnContainer = document.createElement('div');
+    const createBtn = document.createElement('button');
+    const cancelBtn = document.createElement('button');
 
-input.setAttribute('type', 'text');
-input.classList.add('projectInput');
-input.placeholder = 'Project Name';
+    projectForm.classList.add('projectForm');
+    btnContainer.classList.add('btnContainer');
 
-createBtn.textContent = 'Create';
-createBtn.setAttribute('type', 'button');
-createBtn.classList.add('createBtn');
+    input.setAttribute('type', 'text');
+    input.classList.add('projectInput');
+    input.placeholder = 'Project Name';
 
-cancelBtn.textContent = 'Cancel';
-cancelBtn.setAttribute('type', 'button');
-cancelBtn.classList.add('cancelBtn');
+    createBtn.textContent = 'Create';
+    createBtn.setAttribute('type', 'button');
+    createBtn.classList.add('createBtn');
 
-btnContainer.appendChild(createBtn);
-btnContainer.appendChild(cancelBtn);
-projectForm.appendChild(input);
-projectForm.appendChild(btnContainer);
+    cancelBtn.textContent = 'Cancel';
+    cancelBtn.setAttribute('type', 'button');
+    cancelBtn.classList.add('cancelBtn');
 
-export default projectForm;
+    btnContainer.appendChild(createBtn);
+    btnContainer.appendChild(cancelBtn);
+    projectForm.appendChild(input);
+    projectForm.appendChild(btnContainer);
+
+    return { projectForm };
+};
+
+const renderProjectForm = () => {
+    const sidebar = document.querySelector('.sidebar');
+
+    // Append form to projectContent
+    sidebar.appendChild(ProjectForm().projectForm);
+};
+
+function showForm() {
+    const addProjectBtn = document.querySelector('.add-project');
+    const projectForm = document.querySelector('.projectForm');
+
+    // Hide addProjectBtn
+    addProjectBtn.style.display = 'none';
+    // show form
+    projectForm.style.display = 'block';
+}
+
+function hideForm() {
+    const addProjectBtn = document.querySelector('.add-project');
+    const projectForm = document.querySelector('.projectForm');
+
+    // Hide addProjectBtn
+    addProjectBtn.style.display = 'block';
+    // show form
+    projectForm.style.display = 'none';
+};
+
+const eventListeners = () => {
+    const addProjectBtn = document.querySelector('.add-project');
+    addProjectBtn.addEventListener('click',showForm);
+
+    const createBtn = document.querySelector('.createBtn');
+    createBtn.addEventListener('click', hideForm);
+
+    const cancelBtn = document.querySelector('.cancelBtn');
+    cancelBtn.addEventListener('click', hideForm);
+};
+
+
+export  { renderProjectForm, eventListeners };
