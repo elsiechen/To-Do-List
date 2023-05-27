7234e1df7ddda1fea129d8b23524e072b965b32c
elsie <elsiechen915@gmail.com>
2023-05-26 23:24:24 -0400

    Adjust add task div width to fix problem of second word going to second line

--- src/modules/taskForm.js
+++ src/modules/taskForm.js
@@ -5,7 +5,7 @@ const RenderAddTaskBtn = () => {
     const addTaskContainerExist = document.querySelector('.add-task');
     const addTaskContainer = document.createElement('div');
     const svg = document.createElement('div');
-    svg.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px" fill-rule="evenodd"><path fill-rule="evenodd" d="M 11 2 L 11 11 L 2 11 L 2 13 L 11 13 L 11 22 L 13 22 L 13 13 L 22 13 L 22 11 L 13 11 L 13 2 Z"/></svg>
+    svg.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="16px" height="16px" fill-rule="evenodd"><path fill-rule="evenodd" d="M 11 2 L 11 11 L 2 11 L 2 13 L 11 13 L 11 22 L 13 22 L 13 13 L 22 13 L 22 11 L 13 11 L 13 2 Z"/></svg>
     `;
     const addDiv = document.createElement('div');
 
